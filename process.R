f59474d7bd32f59c1f10d5c51690b021310b2884
celsoMattheus <matth3us@users.noreply.github.com>
2018-11-05 21:05:27 -0200

    Conversão do output para modelo do YNAB
    O CSV criado no final do script agora segue o (modelo de importação do YNAB )[https://docs.youneedabudget.com/article/921-importing-a-csv-file]

--- process.R
+++ process.R
@@ -2,20 +2,27 @@ library(tidyverse)
 library(stringr)
 library(lubridate)
 atual <- read_csv('data_2018_11_03.csv') %>% 
-          mutate(
-            categoria = as.factor(categoria), 
-            payee = as.factor(payee),
-            dia = dmy(paste(dia, " 2018")),
-            valor = str_extract(valor, "[\\d,]+"),
-            valor = as.double(str_replace(valor, ',', '.')),
-            valor = ifelse(categoria == 'Pagamento recebido', valor, -valor)
-          )
+  mutate(
+    categoria = as.factor(categoria), 
+    payee = as.factor(payee),
+    Payee = payee,
+    dia   = dmy(paste(dia, " 2018")),
+    Date = dia,
+    valor = str_extract(valor, "[\\d,]+"),
+    valor = as.double(str_replace(valor, ',', '.')),
+    valor = ifelse(categoria == 'Pagamento recebido', valor, -valor)
+    tipo  = ifelse(categoria == 'Pagamento recebido', 'Inflow', 'Outflow'),
+    Memo = tag
+    ) %>%
+  select(-one_of(c('tag', 'dia', 'payee'))) %>% 
+  spread(key = tipo, value = valor, fill = 0) %>% 
+  select(Date, Payee, Memo, Outflow, Inflow)
 
 #3 de novembro: selecionar apenas os dados a partir de 3 de outubro
 atual <- atual %>% 
-          filter(
-            dia > '2018-10-02'
-          )
+  filter(
+    dia > '2018-10-02'
+  )
 
 write_csv(atual, 'data_clean_2018_11_03.csv')
 
