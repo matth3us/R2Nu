671d9a3aa3d43b4dad21d81a16a0f523b1f36953
celsoMattheus <matth3us@users.noreply.github.com>
2018-11-06 15:43:48 -0200

    Criado colunas outflow e inflow
    Colunas outflow e inflow foram criadas, para facilitar importação ao YNAB. A exclusão de colunas desnecessárias foi simplificada.

--- process.R
+++ process.R
@@ -1,22 +1,21 @@
 library(tidyverse)
 library(stringr)
 library(lubridate)
-atual <- read_csv(file.choose()) %>% 
-  mutate(
-    categoria = as.factor(categoria), 
-    payee = as.factor(payee),
-    Payee = payee,
-    dia   = dmy(paste(dia, " 2018")),
-    Date = dia,
-    valor = str_extract(valor, "[\\d,]+"),
-    valor = as.double(str_replace(valor, ',', '.')),
-    valor = ifelse(categoria == 'Pagamento recebido', valor, -valor)
-    tipo  = ifelse(categoria == 'Pagamento recebido', 'Inflow', 'Outflow'),
-    Memo = tag
-    ) %>%
-  select(-one_of(c('tag', 'dia', 'payee'))) %>% 
-  spread(key = tipo, value = valor, fill = 0) %>% 
-  select(Date, Payee, Memo, Outflow, Inflow)
+atual <- read_csv(file.choose())
+atual <- atual %>% 
+          mutate(
+            categoria = as.factor(categoria), 
+            payee = as.factor(payee),
+            Payee = payee,
+            dia  = dmy(paste(dia, " 2018")),
+            Date = dia,
+            valor = str_extract(valor, "[\\d,]+"),
+            valor = as.double(str_replace(valor, ',', '.')),
+            Inflow = ifelse(categoria == 'Pagamento recebido', valor, 0),
+            Outflow = ifelse(categoria == 'Pagamento recebido', 0, valor),
+            Memo = tag
+          )  %>% 
+          select(Date, Payee, Memo, Outflow, Inflow)
 
 #3 de novembro: selecionar apenas os dados a partir de 3 de outubro
 atual <- atual %>% 
