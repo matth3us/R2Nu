242c1196c46c375dad37fd8886ae109554929c21
celsoMattheus <celso.mattheus@gmail.com>
2018-12-09 16:49:50 -0200

    Atualização e simplificação de código de workflow manual.

--- code/process.R
+++ code/process.R
@@ -3,6 +3,22 @@ library(stringr)
 library(lubridate)
 library(jsonlite)
 
+readAllCsv = function(caminho){
+  arquivos = list.files(path = caminho, full.names = TRUE)
+  datalist = lapply(filenames, 
+                    function(x){read_csv(file = x,
+                                         header = TRUE,
+                                         stringsAsFactors = FALSE)})
+  Reduce(function(x,y) {merge(x, y, all = TRUE)}, datalist)
+}
+
+readAllRds = function(caminho){
+  arquivos = list.files(path = caminho, full.names = TRUE)
+  datalist = lapply(filenames, function(x){readRDS(file = x)})
+  Reduce(function(x,y) {merge(x, y, all = TRUE)}, datalist)
+}
+
+# Transformação de dados JSON em dados RDS
 atual <- fromJSON(file.choose())
 atual <- atual %>% 
   mutate(
@@ -20,4 +36,16 @@ atual <- atual %>%
   )  %>% 
   select(id, Date, Payee, Memo, Outflow, Inflow)
 
-saveRDS(atual, file = paste("nubank_",ymd(today()),".RDS",  sep = ""))
+saveRDS(atual, file = paste("./data_rds/nubank_",ymd(today()),".RDS",  sep = ""))
+
+# Atualização de dados
+rdsAtual <- readAllRds('./data_rds/')
+csvAtual <- readAllCsv('./data_csv/')
+newdata  <- dplyr::setdiff(rdsAtual, csvAtual) #e se for vazio?
+newcsv   <- newdata %>% select(-id)
+write_csv(newcsv, paste("./data_csv/csv_",ymd(today()),".csv",  sep = ""))
+write_csv(newdata, paste("./data_ynab/ynab_",ymd(today()),".csv",  sep = ""))
+
+# TODO: funcionando, buscar deletar arquivos e dados supérfluos
+
+
